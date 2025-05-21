a96f1e95ebc4a05307e382c797afb257ae82aba8
unshu221b <142007835+unshu221b@users.noreply.github.com>
2025-05-21 17:51:06 +0800

    feat: update

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,15 +1,16 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Conversation
 from .serializers import ConversationSerializer
+from helpers.myclerk.decorators import api_login_required
+from django.utils.decorators import method_decorator
 
+@method_decorator(api_login_required, name='dispatch')
 class ConversationListCreateView(generics.ListCreateAPIView):
     serializer_class = ConversationSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Make sure this is here
+    # Remove this line since we're using Clerk's authentication
+    # permission_classes = [permissions.IsAuthenticated]  
 
     def get_queryset(self):
-        # Add a safety check
-        if not self.request.user.is_authenticated:
-            return Conversation.objects.none()
         return Conversation.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
