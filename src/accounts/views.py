504af40f64a04c625917d28a0cca07d319bea523
unshu221b <142007835+unshu221b@users.noreply.github.com>
2025-05-21 18:00:32 +0800

    feat: update

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,10 +1,8 @@
 from rest_framework import generics
 from .models import Conversation
 from .serializers import ConversationSerializer
-from helpers.myclerk.decorators import api_login_required
-from django.utils.decorators import method_decorator
 
-@method_decorator(api_login_required, name='dispatch')
+
 class ConversationListCreateView(generics.ListCreateAPIView):
     serializer_class = ConversationSerializer
     # Remove this line since we're using Clerk's authentication
