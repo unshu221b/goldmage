650542cddf55d7cafa4a76f0eec9e7921bf98976
unshu221b <142007835+unshu221b@users.noreply.github.com>
2025-05-22 22:09:37 +0800

    feat: update

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -319,8 +319,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
