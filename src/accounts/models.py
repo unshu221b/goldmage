ba0a64597edb706d78e4bb673f5d0988ef53fa77
unshu221b <142007835+unshu221b@users.noreply.github.com>
2025-07-16 14:47:13 +0800

    feat: update

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -241,13 +241,6 @@ class CreditUsageHistory(models.Model):
 
 class Vault(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    display_name = models.CharField(max_length=150)
-    description = models.TextField(blank=True, null=True)
-    icon = models.CharField(max_length=100, blank=True, null=True)
-    active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.display_name
 
 class Provider(models.Model):
     user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='providers')
